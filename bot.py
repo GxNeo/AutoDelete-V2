d1a47e7ab138d4b5e288836395a46c251d03a663
ɢx  ₪ᴇᴏ <89633118+GxNeo@users.noreply.github.com>
2023-01-01 11:31:53 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import asyncio
 from datetime import datetime, date
 import pytz
+import time
 from os import environ
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
 from pyrogram import Client, filters, idle
